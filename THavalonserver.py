09ca6383b6c7c150fc0b3db0f79e2b6789eb63e9
Natalie Reed <you@example.com>
2019-01-08 13:26:51 -0500

    Small UI changes

--- THavalonserver.py
+++ THavalonserver.py
@@ -4,6 +4,7 @@ import copy
 import os
 import random
 import shutil
+import datetime
 import sys
 sys.path.insert(0, '/home/nreed/.web/THavalon')
 import THavalon
@@ -75,35 +76,67 @@ def index_serve():
 def serve_css(file):
 	return send_from_directory('css', file)
 
-@app.route('/game/')
+@app.route('/game1/')
 def game_list_serve():
-	files = os.listdir('game')
-	return render_template('files.html', files=files)
+        files = os.listdir('game1')
+        return render_template('files.html', files=files)
 
-@app.route('/DoNotOpen/')
-def game_do_not_open():
-	with open('game/DoNotOpen', 'r') as file:
+@app.route('/game2/')
+def game_list_serve2():
+        files = os.listdir('game2')
+        return render_template('files.html', files=files)
+
+@app.route('/game3/')
+def game_list_serve3():
+        files = os.listdir('game3')
+        return render_template('files.html', files=files)
+
+@app.route('/game<number>/ReallyDoNotOpen/')
+def game_really_do_not_open(number):
+	with open('game' + number + '/DoNotOpen', 'r') as file:
                 output = file.read().replace("\n", "<br>")
 	#print(output)
 	return output
-	#return send_from_directory('game', user)
+	#return send_from_directory('game' + number, user)
 
-@app.route('/game/<user>')
-def game_serve(user):
+@app.route('/game<number>/DoNotOpen/')
+def game_do_not_open(number):
+        output = "Are you sure you wish to see DoNotOpen? <a href=\"/game" + number + "/ReallyDoNotOpen\">Click here to view</a>"
+	return output
+
+@app.route('/game<number>/<user>')
+def game_serve(number, user):
 	#assert(os.stat(user))
-	with open('game/' + user, 'r') as file:
-		output = "You are viewing the information of: " + user + "<br><br>" + file.read().replace("\n", "<br>")
+	with open('game' + number + '/' + user, 'r') as file:
+                timestamp = os.path.getmtime('game' + str(number))
+                val = datetime.datetime.fromtimestamp(timestamp)
+                mtime = str(val.strftime('%H:%M:%S %m/%d/%Y'))
+		output = "--- Game " + str(number)
+                output += "---<br>This game was rolled at " + mtime
+                output += "<br><br>You are viewing the information of: " + user + "<br><br>" + file.read().replace("\n", "<br>")
 	#print(output)
 	if user == "DoNotOpen":
-		output = "Are you sure you wish to see DoNotOpen? <a href=\"/DoNotOpen\">Click here to view</a>"
+		output = "Are you sure you wish to see DoNotOpen? <a href=\"/game" + number + "/ReallyDoNotOpen\">Click here to view</a>"
 	return output
 	#return send_from_directory('game', user)
+
+@app.route('/gametime', methods=['GET', 'OPTIONS'])
+#@crossdomain(origin='*')
+def game_get_time():
+        gameroom = request.args.getlist('gameroom')
+        if (len(gameroom) == 1):
+                timestamp = os.path.getmtime('game' + str(gameroom[0]))
+                val = datetime.datetime.fromtimestamp(timestamp)
+                return str(val.strftime('%H:%M:%S %m/%d/%Y'))
+        else:
+                return 'error'
 
 @app.route('/thavalon', methods=['GET', 'OPTIONS'])
 #@crossdomain(origin='*')
 def main():
 	players = request.args.getlist('player')
-	THavalon.run_THavalon(players)
+        gameroom = request.args.getlist('gameroom')
+	THavalon.run_THavalon(players, gameroom)
 	return 'called script'
 			
 if __name__ == "__main__":
